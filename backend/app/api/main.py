118801d74dfb4d3d25fcc8aed953406e14217c32
Lý Văn Minh <138000455+muahe2004@users.noreply.github.com>
2025-11-23 02:55:13 +0700

    Api crud materials (#10)
    * template PR
    
    * api CRUD Materials
    
    ---------
    
    Co-authored-by: minhBlueOC <minh.ly@blueoc.tech>

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.api.routes import (
     healthCheck,
     products,
+    materials
 )
 
 api_router = APIRouter()
 
 api_router.include_router(healthCheck.router, prefix="/healthCheck", tags=["healthCheck"])
 api_router.include_router(products.router, prefix="/products", tags=["Products"])
+api_router.include_router(materials.router, prefix="/materials", tags=["materials"])
